d8ed7ede049469e7b29531bca4657d19c0434252
Fazle Rabbi <35403788+irabbi360@users.noreply.github.com>
2024-04-19 00:45:17 +0600

    tailwind & products add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      title: 'Nuxt Tailwind',
+      meta: [
+        {name: 'description', content: 'Everything about Nuxt 3'}
+      ],
+      link: [
+        {rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons'}
+      ]
+    }
+  },
+  modules: ['@nuxtjs/tailwindcss']
 })
---
+++ pages/products/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="grid grid-cols-4 gap-5">
+      <div
+        v-for="p in products"
+        class="flex flex-col shadow-md bg-white p-6 rounded-md"
+        >
+        <ProductCard :product="p" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  //  fetch the products
+  const { data: products } = await useFetch('https://fakestoreapi.com/products')
+
+  definePageMeta({
+    layout: "products",
+  })
+
+  useHead({
+    title: 'Nuxt Tailwind | Merch',
+    meta: [
+      { name: 'description', content: 'Nuxt 3 Merch'}
+    ]
+  })
+</script>
